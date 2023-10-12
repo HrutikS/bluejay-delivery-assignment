c4be1f839b57f0b9c5f339c7abbb28692253d03b
Hrutik S <134339154+HrutikS@users.noreply.github.com>
2023-10-12 19:42:40 +0530

    Update Condition3MoreThanFourteen.java

--- src/dem/main/Condition3MoreThanFourteen.java
+++ src/dem/main/Condition3MoreThanFourteen.java
@@ -10,37 +10,35 @@ public class Condition3MoreThanFourteen extends Condition1ConsecutiveSeven{
 	//Method to get Employees who has worked for more than 14 hours in a single shift
 	public void getEmployeesWorkingMoreThanFourteenHoursSingleShift() {
 		try {
-            Connection connection = DriverManager.getConnection(jdbcURL, username, password);
-            if (connection != null) {		
-                System.out.println("Connected to the database.");
+            		Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+            		if (connection != null) {		
+                		System.out.println("Connected to the database.");
 
-                // Query to retrieve data
-                String query = "SELECT * FROM employee";
-                PreparedStatement statementForAll = connection.prepareStatement(query);
-                ResultSet resultSetForAll = statementForAll.executeQuery();
+                		// Query to retrieve data
+                		String query = "SELECT * FROM employee";
+                		PreparedStatement statementForAll = connection.prepareStatement(query);
+                		ResultSet resultSetForAll = statementForAll.executeQuery();
                 
-                //Traversing resultSet
-                while(resultSetForAll.next()) {
+                		//Traversing resultSet
+                		while(resultSetForAll.next()) {
                 	
-                	String timeCardHours = resultSetForAll.getString("Timecard Hours (as Time)");
-                	
-                	if(!timeCardHours.isEmpty()) {
-                		String[] hours = timeCardHours.split(":");
-                    	int hoursInInt = Integer.parseInt(hours[0]);
-                    	if(hoursInInt > 14) {
-                    		System.out.println("Employees who worked for more than 14hrs in single shift:");
-                    		System.out.println(resultSetForAll.getString("Employee Name")
-    											+" - "
-    											+resultSetForAll.getString("Position Status"));
-                    	}
-                	}
-                }
-            }
-            // Close the database connection
-            connection.close();
+	                		String timeCardHours = resultSetForAll.getString("Timecard Hours (as Time)");
+        	        	
+                			if(!timeCardHours.isEmpty()) {
+                				String[] hours = timeCardHours.split(":");
+                    				int hoursInInt = Integer.parseInt(hours[0]);
+                    				if(hoursInInt > 14) {
+	                    				System.out.println("Employees who worked for more than 14hrs in single shift:");
+        	            				System.out.println(resultSetForAll.getString("Employee Name")+" - "+resultSetForAll.getString("Position Status"));
+                    				}
+                			}
+                		}
+            		}
+            		// Close the database connection
+            		connection.close();
             
 		}catch (Exception e) {
-        	e.printStackTrace();
+			e.printStackTrace();
 		}            
 	}
 }
