4422a05fe481a94125933ddbd4a1f8db3260c207
Hrutik S <134339154+HrutikS@users.noreply.github.com>
2023-10-12 19:50:16 +0530

    Update Condition1ConsecutiveSeven.java

--- src/dem/main/Condition1ConsecutiveSeven.java
+++ src/dem/main/Condition1ConsecutiveSeven.java
@@ -32,90 +32,90 @@ public class Condition1ConsecutiveSeven {
 	public void getEmployeesWorkingSevenConsecutiveDays(){
 		
 		try {
-            Connection connection = DriverManager.getConnection(jdbcURL, username, password);
-            if (connection != null) {		
-                System.out.println("Connected to the database.");
+            		Connection connection = DriverManager.getConnection(jdbcURL, username, password);
+            		if (connection != null) {		
+                		System.out.println("Connected to the database.");
 
-                // Query to retrieve all data
-                String query = "SELECT * FROM employee";
-                PreparedStatement statementForAll = connection.prepareStatement(query);
-                ResultSet resultSetForAll = statementForAll.executeQuery();
+                		// Query to retrieve all data
+                		String query = "SELECT * FROM employee";
+                		PreparedStatement statementForAll = connection.prepareStatement(query);
+                		ResultSet resultSetForAll = statementForAll.executeQuery();
+		                
+                		// Query to retrieve distinct data 
+		                String queryForUniqueEmployee = "SELECT DISTINCT `Employee Name`, `Position Status` FROM employee;";
+		                PreparedStatement statementForUniqueEmployee = connection.prepareStatement(queryForUniqueEmployee);
+		                ResultSet resultSetForUniqueEmployee = statementForUniqueEmployee.executeQuery();
                 
-                // Query to retrieve distinct data 
-                String queryForUniqueEmployee = "SELECT DISTINCT `Employee Name`, `Position Status` FROM employee;";
-                PreparedStatement statementForUniqueEmployee = connection.prepareStatement(queryForUniqueEmployee);
-                ResultSet resultSetForUniqueEmployee = statementForUniqueEmployee.executeQuery();
-                
-                //Saving resultSetForUniqueEmployee data into list.
-                while(resultSetForUniqueEmployee.next()) {
-                	listOfUniqueEmployees.add(resultSetForUniqueEmployee.getString("Employee Name"));
-                	listOfUniqueEmployeesWithPoisitionStatus.put(resultSetForUniqueEmployee.getString("Employee Name"),
+		                //Saving resultSetForUniqueEmployee data into list.
+		                while(resultSetForUniqueEmployee.next()) {
+                			listOfUniqueEmployees.add(resultSetForUniqueEmployee.getString("Employee Name"));
+                			listOfUniqueEmployeesWithPoisitionStatus.put(resultSetForUniqueEmployee.getString("Employee Name"),
                 												resultSetForUniqueEmployee.getString("Position Status"));
-                }
-                	
-                //Traversing resultSet
-                while (resultSetForAll.next()) {
-
-                	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm a");
-                	
-                	String currEmployeeName = resultSetForAll.getString("Employee Name");
-                	String prevEmployeeName = listOfUniqueEmployees.get(index);
-                	
-                	if(!currEmployeeName.equals(prevEmployeeName)) {
-                		index++;
-                		prevEmployeeName = listOfUniqueEmployees.get(index);
-                		prevDay = 0;
-                	}
-                	
-                	//Traversing for same employee
-                	while(prevEmployeeName.equals(currEmployeeName)) {
-
-                		String currTime = resultSetForAll.getString("Time");
-                		
-                		if(currTime.isEmpty()) {
-                			index++;
-                			break;
                 		}
-                		
-                		LocalDateTime currDateTime = LocalDateTime.parse(currTime, formatter);
-                		
-                		if(!currTime.isEmpty()) {
-                			currDay = currDateTime.getDayOfMonth();
-                			if(prevDay == 0) {
-                				prevDay = currDay;
-                			}
-                			if(currDay == prevDay) {
-                				break;
-                			}
-                			if(currDay == prevDay+1) {
-                				consecutiveDays++;
-                				prevDay = currDay;
-                			}else if(currDay != prevDay+1 && currDay > prevDay){
-                				consecutiveDays = 0;
-                				prevDay = currDay;
-                			}
-                		}
-                		
-                		//Adding employee names to consecutiveWorkers list.
-                		if(consecutiveDays == 7) {
-                    		consecutiveWorkers.add(currEmployeeName);
-                    	}
-                	}
-                }       
-            }
+                	
+                		//Traversing resultSet
+                		while (resultSetForAll.next()) {
 
-            //Print employees who worked for 7 consecutive days
-            System.out.println("Employees who worked for 7 consecutive days:");
-            for(String worker : consecutiveWorkers) {
-            	System.out.println(worker+" - "+listOfUniqueEmployeesWithPoisitionStatus.get(worker));
-            }
+                			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy hh:mm a");
+                	
+                			String currEmployeeName = resultSetForAll.getString("Employee Name");
+                			String prevEmployeeName = listOfUniqueEmployees.get(index);
+	                	
+        		        	if(!currEmployeeName.equals(prevEmployeeName)) {
+                				index++;
+                				prevEmployeeName = listOfUniqueEmployees.get(index);
+                				prevDay = 0;
+                			}
+                	
+		                	//Traversing for same employee
+                			while(prevEmployeeName.equals(currEmployeeName)) {
 
-            // Close the database connection
-            connection.close();
-        }
-        catch (Exception e) {
-            	e.printStackTrace();
-        }
+                				String currTime = resultSetForAll.getString("Time");
+	                		
+        		        		if(currTime.isEmpty()) {
+                					index++;
+		                			break;
+                				}
+                		
+                				LocalDateTime currDateTime = LocalDateTime.parse(currTime, formatter);
+                		
+                				if(!currTime.isEmpty()) {
+                					currDay = currDateTime.getDayOfMonth();
+                					if(prevDay == 0) {
+                						prevDay = currDay;
+                					}
+                					if(currDay == prevDay) {
+                						break;	
+                					}
+                					if(currDay == prevDay+1) {
+                						consecutiveDays++;
+                						prevDay = currDay;
+                					}else if(currDay != prevDay+1 && currDay > prevDay){
+                						consecutiveDays = 0;
+                						prevDay = currDay;
+                					}
+                				}
+                		
+                				//Adding employee names to consecutiveWorkers list.
+                				if(consecutiveDays == 7) {
+                    					consecutiveWorkers.add(currEmployeeName);
+                    				}
+                			}
+                		}       
+            		}
+
+            		//Print employees who worked for 7 consecutive days
+            		System.out.println("Employees who worked for 7 consecutive days:");
+            		for(String worker : consecutiveWorkers) {
+            			System.out.println(worker+" - "+listOfUniqueEmployeesWithPoisitionStatus.get(worker));
+            		}
+
+            		// Close the database connection
+            		connection.close();
+        	}
+        	catch (Exception e) {
+            		e.printStackTrace();
+        	}
 		
 	}
 	
